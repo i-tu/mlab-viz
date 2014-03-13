eeb1c71c615dfd7df66841d7765db4019547eb0c
Ian Tuomi <ian@dhcp-87-69.eduroam.aalto.fi>
2014-03-13 19:44:16 +0200

    workworkwork

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var margin = {top: 50, right: 50, bottom: 50, left: 50};
-var width = $(window).width() - margin.left - margin.right;
+var width = $("#content").width() - margin.left - margin.right;
 var height = $(window).height() - margin.top - margin.bottom;
 
 function profileText( d ) {
@@ -14,15 +14,15 @@ var svgContainer = d3.select("#content")
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                    //.append("g")
+                    //.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                     .call(profileTooltip);
 
 queue().defer(d3.json, 'data/data.json')
        .await(ready);
 
 function ready(error, jsonData){
-    
+    console.log(jsonData);
     function translation(d){
         return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
     };
@@ -38,11 +38,11 @@ function ready(error, jsonData){
                    .range([ height, 0 ]);
 
 
-    function createInstances( objects, t ){
-        return svgContainer.selectAll(t)
+    function createInstances( objects, type ){
+        return svgContainer.selectAll(type)
                            .data(objects)
                            .enter()
-                           .append(t)
+                           .append(type)
                            .attr("x", function(d){return xScale(d.x);})
                            .attr("y", function(d){return yScale(d.y);})
                            .attr("transform", translation);
@@ -61,30 +61,69 @@ function ready(error, jsonData){
         }
         else if(object.cat <= 18){
             b = 255 - (object.cat-18)*10;
-    }
+        }
 
         return "rgb("+r+","+g+","+b+")";
     };
 
     function createStudents (object){
-        object.append("circle")
-              .attr("class", "student")
-              .attr("r", function (d) { return 10; })
+        object.attr("class", "student")
+              .attr("category", function(d){return d.category;})
+              .attr("r", 15)//function (d) { return 10; })
               .style("fill", colorCode )
-              .style("stroke", "rgb(0,0,0)");
-
-        svgContainer.selectAll("circle")
+              .style("stroke", "rgb(0,0,0)")
+              .attr("opacity", 0)
               .on('mouseover', profileTooltip.show)
-              .on('mouseout', profileTooltip.hide);
+              .on('mouseout', profileTooltip.hide)
+              .on('click', clicky );
     };
 
+    function clicky(d){
+        console.log(d.name);
+    };
+
+    var changeColor = (function(){
+        return function(){
+            d3.select(this).style("fill", "rgb(255,0,0)");
+        }
+    })();
+    
     function createSkills(object){
         object.attr("text-anchor", "middle")
               .html( function(d){return d.name;} );
     };
-    
-    var students = createInstances( jsonData.students, "g");
+
+
+    function selectClass( c ) {
+        d3.select("#" + c);
+    };
+
+    function createSidebar(d){
+
+        d3.select("#buttons")
+          .selectAll("button")
+          .data(d)
+          .enter() 
+          .append("button")
+          .attr("type", "button")
+          .attr("class", "btn btn-default")
+          .text(function(d){ return d; })
+     //     .on("click", make);
+    };
+
+
+    var students = createInstances( jsonData.students, "circle");
     createStudents(students);
+    students.transition().duration(1000).attr("opacity", 1).attr("r", 10);
+
+
     var skills  = createInstances( jsonData.skills, "text");
     createSkills(skills);
+    
+    createSidebar( jsonData.classes );
+
+    console.log(students.select(function(d){return d.category.equals("ma10");}))
+        //.style("fill","rgb(255,255,0)"));
+    
+
 };
